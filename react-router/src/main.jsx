feeda7a68a90a1d734dd28e8b0ca720d8c4e1140
tamzidpeace <tamjedpeace@gmail.com>
2025-11-11 07:23:30 +0600

    feat(routing): update routing structure with react-router integration
    This commit enhances the routing setup by integrating `react-router` for better navigation. It adds a new `react-router` dependency and restructures the `main.jsx` file to utilize `createBrowserRouter`, defining nested routes for the Home, About, and Contact components. The package-lock and package.json files are also updated to reflect the new dependency.

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
 
-createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      { index: true, Component: Home },
+      { path: "about", Component: About },
+      { path: "contact", Component: Contact },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
